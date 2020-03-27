2ffe27fe08af610d2d3197809a4e4d95354c315c
YakuI7 <52265864+YakuI7@users.noreply.github.com>
2020-03-27 17:45:36 -0400

    Added product url w/ slug. Added discounted price to render

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import item_list, checkout, products
+from .views import checkout, ItemDetailView, HomeView
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name='item-list'),
+    path('', HomeView.as_view(), name='home'),
     path('checkout/', checkout, name="checkout"),
-    path('products/', products, name="products")
+    path('product/<slug>', ItemDetailView.as_view(), name='product'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render
 from .models import Item
+from django.views.generic import ListView, DetailView
 # Create your views here.
-
-
-def item_list(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, "home-page.html", context)
-
-
-def checkout(request):
-    context = {
-
-    }
-    return render(request, "checkout-page.html", context)
 
 
 def products(request):
     context = {
 
     }
-    return render(request, "product-page.html", context)
+    return render(request, "products.html", context)
+
+
+def checkout(request):
+    context = {
+
+    }
+    return render(request, "checkout.html", context)
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = "home.html"
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product.html"
