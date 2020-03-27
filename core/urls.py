3ed4746901edceff4b336346b3bafe1d5069ebfd
YakuI7 <52265864+YakuI7@users.noreply.github.com>
2020-03-27 16:15:20 -0400

    Added templates to the html pages

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import item_list
+from .views import item_list, checkout, products
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name='item-list')
+    path('', item_list, name='item-list'),
+    path('checkout/', checkout, name="checkout"),
+    path('products/', products, name="products")
+
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,17 @@ def item_list(request):
         'items': Item.objects.all()
     }
     return render(request, "home-page.html", context)
+
+
+def checkout(request):
+    context = {
+
+    }
+    return render(request, "checkout-page.html", context)
+
+
+def products(request):
+    context = {
+
+    }
+    return render(request, "product-page.html", context)
