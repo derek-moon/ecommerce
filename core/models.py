50a008b1ef6beb3859fc1a0c4ec5b38174de4463
YakuI7 <52265864+YakuI7@users.noreply.github.com>
2020-03-28 12:30:35 -0400

    Add to cart working. OrderItem added with quantities. Order created w/ ordered = False

--- core/models.py
+++ core/models.py
@@ -22,6 +22,7 @@ class Item(models.Model):
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
+    description = models.TextField()
 
     def __str__(self):
         return self.title
@@ -31,12 +32,22 @@ class Item(models.Model):
             'slug': self.slug
         })
 
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
 
 class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.title
+        return f"{self.quantity} of {self.item.title}"
 
 
 class Order(models.Model):
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import checkout, ItemDetailView, HomeView
+from .views import (
+    checkout,
+    ItemDetailView,
+    HomeView,
+    add_to_cart
+)
 
 app_name = 'core'
 
@@ -7,5 +12,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('checkout/', checkout, name="checkout"),
     path('product/<slug>', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>', add_to_cart, name="add-to-cart")
 
 ]
